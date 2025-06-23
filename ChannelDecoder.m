48bdd1a06d8b8d0cc67194fec59e55fab1a8643e
Paasman, P.M.N. (Pieter, Student B-EE) <p.m.n.paasman@student.utwente.nl>
2025-06-23 17:33:33 +0200

    Make Hamming work for arbitrary code rates

--- ChannelDecoder.m
+++ ChannelDecoder.m
@@ -13,7 +13,7 @@ function y = ChannelDecoder( u , n , k , EncType )
         % k = uncoded = 4
         % n = coded = 7
         p = n - k; % Number of parity bits
-        H = hammgen(p); % Hamming parity check matrix
+        [H, G] = hammgen(p); % Hamming parity check matrix
         R = reshape(u, n, []); % Received codewords
         S = mod(H * R, 2); % Syndromes
         S_tab = syndtable(H);  % Syndrome table
@@ -21,14 +21,8 @@ function y = ChannelDecoder( u , n , k , EncType )
         % C = S * u; % Correction table (binary)
         correction_table = S_tab(1+S_int, :)';
         corrected_code = mod(correction_table + R, 2);
-        Retrieval_mat = [
-        0 0 0 0;
-        0 0 0 0;
-        0 0 0 0;
-        1 0 0 0;
-        0 1 0 0;
-        0 0 1 0;
-        0 0 0 1]';
+        Retrieval_mat = G;
+        Retrieval_mat(:, 1:p) = 0;
         y = reshape(Retrieval_mat * corrected_code, 1, []);
     end
     
