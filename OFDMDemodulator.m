a2c3840b4809bb2a74b4ad501b3ec425e2a77d94
rolf-tk <149889957+rolf-tk@users.noreply.github.com>
2025-06-24 09:44:25 +0200

    Update OFDMDemodulator.m: implemented

--- OFDMDemodulator.m
+++ OFDMDemodulator.m
@@ -1,41 +1,37 @@
 % OFDM demodulator
 function y = OFDMDemodulator( u , SamplePerSymbol , NumOfSubcarriers , BitPerSymbol , Rb )
-    % Read the following CAREFULLY!
+	% Read the following CAREFULLY!
 	% Since NumOfSubcarriers = 4, we have 4 subcarrier symbols.
 	% SamplePerSymbol should be interpreted as number of samples per subcarrier symbol.
-    % BitPerSymbol should be interpreted as number of channel bits per subcarrier symbol.
-    % Subcarrier symbol interval = OFDM symbol interval.
+    	% BitPerSymbol should be interpreted as number of channel bits per subcarrier symbol.
+    	% Subcarrier symbol interval = OFDM symbol interval.
 	% Rb = output bit rate of overall system
 	
-	% Rs = Insert your code here                        % output symbol rate of overall system
-    % Rsub = Insert your code here                      % subcarrier symbol rate
-    % SubcarrierSpacing = Insert your code here         % subcarrier spacing
-    % SubcarrierFreqVector = Insert your code here      % frequencies of all subcarriers (note: NumOfSubcarriers = 4)
+	Rs = Rb/BitPerSymbol;               % input symbol rate of overall system
+    	Rsub = Rs/NumOfSubcarriers;         % subcarrier symbol rate
+    	SubcarrierSpacing = Rsub;           % subcarrier spacing
+    	SubcarrierFreqVector = -(NumOfSubcarriers-1)/2*SubcarrierSpacing:SubcarrierSpacing:(NumOfSubcarriers-1)/2*SubcarrierSpacing;
+    	% frequencies of all subcarriers (note: NumOfSubcarriers = 4)
+
+    	Tofdm = 1/Rsub;                                         % OFDM symbol interval
+    	Fs = SamplePerSymbol*Rsub;                                % sampling frequency
+    	Ts = 1/Fs;                                              % sampling interval
+    	Tframe = length(u)*Ts; % the time needed for all bits to be sent
+    	t = 0:Ts:Tframe-Ts;                                     % time vector
+
+     	%Preallocate
+    	sequences=zeros(NumOfSubcarriers,length(u)/SamplePerSymbol);
+    	mixer=zeros(NumOfSubcarriers,length(u));
+     
+    	for i=1:NumOfSubcarriers
+        	mixer(i,:)=u.*exp(-1*1i*2*pi*SubcarrierFreqVector(i)*t);
+        	sequences(i,:)=intdump(mixer(i,:),SamplePerSymbol);
+    	end 
+     	
+    	% Reshape output to one array
+     	[m,n]=size(sequences);
+    	y=reshape(sequences,1,m*n);
     
-    % Tofdm = Insert your code here                     % OFDM symbol interval
-    % Fs = Insert your code here                        % sampling frequency
-    % Ts = Insert your code here                        % sampling interval
-    % Tframe = Insert your code here                    % the time needed for all bits to be received
-    % t = Insert your code here                         % time vector
-    
-    % 1st branch for 1st subcarrier (Insert your code here)
-    %     First do multiplication with complex exponential corresponding to 1st subcarrier.
-    %     Then use multiplication output as "xxx" in y1 below
-    %     y1 = intdump( xxx , SamplePerSymbol );
-    
-    
-    % 2nd branch for 2nd subcarrier (Insert your code here)
-    
-    
-    % 3rd branch for 3rd subcarrier (Insert your code here)
-    
-    
-    % 4th branch for 4th subcarrier (Insert your code here)
-    
-    
-    % parallel to serial (Insert your code here)
-    
-    
-    % To ensure dimension compatibility
-    y = y(:).';
+   	 % To ensure dimension compatibility
+    	y = y(:).';
 end
